1a202f474506bfbd18d9068ae34f774d9e3d99da
Eric Teixeira <ertfly@gmail.com>
2023-04-16 23:53:02 -0300

    fix docker node install and start

--- front/src/index.js
+++ front/src/index.js
@@ -1,7 +1,4 @@
 import reportWebVitals from './reportWebVitals';
-import GoogleMap from './common/containers/GoogleMaps'
-
-window.google = GoogleMap().then(_ => { });
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
